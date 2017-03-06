db2f5c695cbec77b44323cdcc5a930a99dd21fba
Sarbjit Singh <srbcheema1@gmail.com>
2017-03-06 19:24:23 +0530

    initial commit

---
+++ src/blackVsWhite/BlackVsWhite.java
@@ -0,0 +1,14 @@
+package blackVsWhite;
+
+/**
+ *
+ * @author srb
+ */
+public class BlackVsWhite {
+
+    public static void main(String[] args) {
+        MainFrame mainframe = new MainFrame();
+        mainframe.setVisible(true); 
+    }
+    
+}
